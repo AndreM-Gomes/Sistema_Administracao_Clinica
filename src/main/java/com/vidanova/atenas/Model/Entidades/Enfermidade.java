2a62415e13231091fc1004b285adb2e6b4e58fd5
André Monteiro Gomes <andre.montero702@gmail.com>
2019-11-20 18:19:05 -0200

    Adicionando validação de campos na entidade

--- src/main/java/com/vidanova/atenas/Model/Entidades/Enfermidade.java
+++ src/main/java/com/vidanova/atenas/Model/Entidades/Enfermidade.java
@@ -1,8 +1,16 @@
 package com.vidanova.atenas.Model.Entidades;
 
+import javax.validation.constraints.*;
+
 public class Enfermidade {
     int idEnfermidade;
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Z]\\d{2,3}$")
     String CID;
+
+    @NotBlank
+    @Size(min = 6,max = 260)
     String nome;
 
     public Enfermidade(int idEnfermidade,String CID,String nome){
